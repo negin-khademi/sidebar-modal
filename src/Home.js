e30257eee68444f4dd286d17c0a16899707e97d2
Negin <negin.khademi@gmail.com>
2024-12-29 18:13:15 +0330

    global context

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,8 @@
+import { useGlobalContext } from './context';
+
 const Home = () => {
+  const { openSidebar, openModal } = useGlobalContext();
+  console.log(openSidebar, openModal);
   return <div>Home</div>;
 };
 
